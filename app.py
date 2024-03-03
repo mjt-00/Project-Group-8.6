e597bf3368485e5b2cb6ad6e0c979166a6c4bca9
mjt-00 <47736695+mjt-00@users.noreply.github.com>
2024-03-03 18:52:39 +0100

    Created new feature file 01

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import streamlit as st
 
+from Feature_01 import return_even
+
+original_list = [i for i in range(10)]
+
+even_list = return_even(original_list)
+
 st.write("It worked")
 st.button("click me")
