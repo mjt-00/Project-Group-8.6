f88c9473e10219e2bae4ebabefbbbc4e69a604da
mjt-00 <47736695+mjt-00@users.noreply.github.com>
2024-03-03 18:56:42 +0100

    added second feature

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import streamlit as st
 
 from Feature_01 import return_even
+from Feature_02 import return_odd
 
 original_list = [i for i in range(10)]
 
 even_list = return_even(original_list)
 
+odd_list = return_odd(original_list)
+
 st.write("It worked")
 st.button("click me")
 st.write(even_list)
+st.write(odd_list)
